a68c4b192c494fb1af809ae26f87688df327b2d5
Filip Jukic <filip@jukic.me>
2013-07-19 15:26:50 -0400

    disabled django compressor for now

--- settings_openshift.py
+++ settings_openshift.py
@@ -4,6 +4,6 @@ from settings import *
 PROJECT_ROOT = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'mediathread', 'mediathread')
 
 COMPRESS_ROOT = os.path.join(PROJECT_ROOT, 'media')
-
+COMPRESS_ENABLED = False
 
 SECRET_KEY = "$#bc!$782^y76@vs3lr+w^qx)&qsuic*ycz%ta_f^cu(1+4($x"
