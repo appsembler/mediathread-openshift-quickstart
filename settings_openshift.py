531833a2af780ea583c67fe5455491dac0980ca1
Filip Jukic <filip@jukic.me>
2013-07-19 15:53:35 -0400

    removed compressor settings for now

--- settings_openshift.py
+++ settings_openshift.py
@@ -3,7 +3,10 @@ from settings import *
 
 PROJECT_ROOT = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'mediathread', 'mediathread')
 
-COMPRESS_ROOT = os.path.join(PROJECT_ROOT, 'media')
+COMPRESS_ROOT = ""
 COMPRESS_ENABLED = False
+COMPRESS_URL = ""
+COMPRESS_ROOT = ""
+COMPRESS_PARSER = ""
 
 SECRET_KEY = "$#bc!$782^y76@vs3lr+w^qx)&qsuic*ycz%ta_f^cu(1+4($x"
