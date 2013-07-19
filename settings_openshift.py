09d4cb4e6d1e83ce2c7b64bef715ab72cfdfe264
Filip Jukic <filip@jukic.me>
2013-07-19 15:19:46 -0400

    setting compress_url

--- settings_openshift.py
+++ settings_openshift.py
@@ -1,3 +1,9 @@
+import os
 from settings import *
+
+PROJECT_ROOT = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'mediathread', 'mediathread')
+
+COMPRESS_ROOT = os.path.join(PROJECT_ROOT, 'media')
+
 
 SECRET_KEY = "$#bc!$782^y76@vs3lr+w^qx)&qsuic*ycz%ta_f^cu(1+4($x"
