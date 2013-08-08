c0f606e60ce0fb9f2c2223ec7552bbe45bdf3fa9
Filip Jukic <filip@jukic.me>
2013-08-08 17:23:04 -0400

    added mandrill as a SMTP server

--- settings_openshift.py
+++ settings_openshift.py
@@ -71,6 +71,11 @@ CUSTOMERIO_API_KEY = get_env_variable('CUSTOMERIO_API_KEY')
 MAILCHIMP_API_KEY = get_env_variable('MAILCHIMP_API_KEY')
 MAILCHIMP_REGISTRATION_LIST_ID = get_env_variable('MAILCHIMP_REGISTRATION_LIST_ID')
 
+EMAIL_HOST = 'smtp.mandrillapp.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = get_env_variable('MANDRILL_USERNAME')
+EMAIL_HOST_PASSWORD = get_env_variable('MANDRILL_API_KEY')
+
 # Sentry/Raven config
 RAVEN_CONFIG = {
     'dsn': get_env_variable('SENTRY_DSN'),
