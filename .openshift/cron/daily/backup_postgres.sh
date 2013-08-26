af891c61c1311582987e1c214faed6813a438cc9
Filip Jukic <filip@jukic.me>
2013-08-26 11:41:16 -0400

    created a folder for db backups, updated the path in backup script

--- .openshift/cron/daily/backup_postgres.sh
+++ .openshift/cron/daily/backup_postgres.sh
@@ -3,5 +3,5 @@
 # by Skye Book <skye.book@gmail.com>
  
 NOW="$(date +"%Y-%m-%d")"
-FILENAME="$OPENSHIFT_DATA_DIR/$OPENSHIFT_APP_NAME.$NOW.backup.sql.gz"
+FILENAME="$OPENSHIFT_DATA_DIR/db_backups/$OPENSHIFT_APP_NAME.$NOW.backup.sql.gz"
 /opt/rh/postgresql92/root/usr/bin/pg_dump -h $OPENSHIFT_POSTGRESQL_DB_HOST -p $OPENSHIFT_POSTGRESQL_DB_PORT -F c $OPENSHIFT_APP_NAME > $FILENAME
