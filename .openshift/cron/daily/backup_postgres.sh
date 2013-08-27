66970be2666c9d8ff832f5270be008767bd78954
Filip Jukic <filip@jukic.me>
2013-08-27 11:15:26 -0400

    db backups now uploaded to S3

--- .openshift/cron/daily/backup_postgres.sh
+++ .openshift/cron/daily/backup_postgres.sh
@@ -5,3 +5,5 @@
 NOW="$(date +"%Y-%m-%d")"
 FILENAME="$OPENSHIFT_DATA_DIR/db_backups/$OPENSHIFT_APP_NAME.$NOW.backup.sql.gz"
 /opt/rh/postgresql92/root/usr/bin/pg_dump -h $OPENSHIFT_POSTGRESQL_DB_HOST -p $OPENSHIFT_POSTGRESQL_DB_PORT -F c $OPENSHIFT_APP_NAME > $FILENAME
+cd $OPENSHIFT_DATA_DIR/s3cmd 
+s3cmd -c .s3cfg put $FILENAME s3://mediathreadbackups
